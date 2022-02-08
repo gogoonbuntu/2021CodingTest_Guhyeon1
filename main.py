8414cf34edd53af95ccf58e792f535b6fedf7094
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2022-02-08 10:31:35 +0000

    성공

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# N 은 맵 크기 NxN
+# D 는 움직임 방향. LRUD 중 하나.
+# 크기 밖으로 나가면 무시
+
 def main ():
 	N = int(input())
 	print("N = ", N)
@@ -17,3 +21,5 @@ def main ():
 		print("(%d, %d)", x, y) 
 
 main()
+
+# 방향에 주의할 것.
